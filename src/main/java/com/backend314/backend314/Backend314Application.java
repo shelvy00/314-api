a986c43a567f496ed842b5aff3db9dfa11a1a109
Derrick Milliot <merlot5390@gmail.com>
2024-03-11 20:30:09 -0500

    initcommit

---
+++ src/main/java/com/backend314/backend314/Backend314Application.java
@@ -0,0 +1,13 @@
+package com.backend314.backend314;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend314Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backend314Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend314/backend314/Backend314ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend314.backend314;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend314ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
